d817b85f76410916448484aa87351b5add3b0ef5
Angelo Hervis <angelo.hervis@gmail.com>
2024-02-15 21:30:37 -0300

    Initial commit

---
+++ Aula 01/script.js
@@ -0,0 +1,24 @@
+let nome = "Ângelo"
+let anoNascimento = 2005
+
+console.log(nome)
+console.log(anoNascimento)
+
+let a,b
+
+a = 1
+b = 2
+c = 0
+
+console.log(a)
+console.log(b)
+
+c = a
+a = b
+b = c
+
+console.log(a)
+console.log(b)
+
+
+
