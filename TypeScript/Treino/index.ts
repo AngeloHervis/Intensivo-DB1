6f4915648ce0a4ed55f7b93e3d58c87cd1713829
Angelo Hervis <angelo.hervis@gmail.com>
2024-03-25 18:12:25 -0300

    att

---
+++ TypeScript/Treino/index.ts
@@ -0,0 +1,6 @@
+// Ex 01:Escreva uma função em TypeScript chamada adicionar que recebe dois parâmetros do tipo number e retorna a soma desses números. Certifique-se de definir os tipos de parâmetros e de retorno explicitamente.
+function adicionar(a: number, b: number): number{
+    return a+b;
+}
+
+//Ex02:Crie uma interface chamada Usuario com as propriedades nome (string) e idade (number). Em seguida, escreva uma função chamada cumprimentarUsuario que aceita um parâmetro do tipo Usuario e retorna uma saudação personalizada usando as propriedades desse objeto.
